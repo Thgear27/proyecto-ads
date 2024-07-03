2498a7ec968043751034055331cac05c115a1cd8
Fernando Vegas <90532274+Thgear27@users.noreply.github.com>
2024-07-02 22:30:57 -0500

    feat: protect routes (#9)

--- src/moduloAlmacen/getRegistrarProductoAlmacen.php
+++ src/moduloAlmacen/getRegistrarProductoAlmacen.php
@@ -9,6 +9,13 @@ if (!isset($_SESSION['autenticado'])) {
     exit();
 }
 
+$rol = $_SESSION['rol'];
+
+if ($rol != "almacen" && $rol != "administrador") {
+  header('Location: /moduloSeguridad/indexPanelPrincipal.php');
+  exit();
+}
+
 $nombreCampoErroneo = '';
 $mensajeError = '';
 
